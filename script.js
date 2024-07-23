05ef3d1c20f16378f1c5e48514cb6061aeb3c4c8
Nikesh Borkar <99666800+NikeshBorkar@users.noreply.github.com>
2024-07-23 21:09:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function firstWord(s) {
   // your code here
+	let n=s.length
+	let ans=""
+	for(let i=0;i<n;i++){
+		if(s[i]==" "){
+			break;
+		}
+		ans+=s[i]
+	}
+	return ans
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
+const s = prompt("Enter String:");  
 alert(firstWord(s));
